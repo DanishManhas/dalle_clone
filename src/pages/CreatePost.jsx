40905b5fc904d20b79d788e19713ae15862b5363
Danish Manhas <manhasd6@gmail.com>
2023-12-02 18:35:05 +0530

    fix utils import gaffe in CreatePost page

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { preview } from "../assets";
-import { getRandmPrompt } from ".utils";
+import {getRandomPrompt} from './../utils'
 import { FormField, Loader } from "../components";
 const CreatePost = () => {
   const navigate = useNavigate();
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,25 @@
 import { surpriseMePrompts } from "../constants";
 
-export function getRandomPrompt(prompt) {
+function getRandomPrompt(prompt){
   const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
   const randomPrompt = surpriseMePrompts(randomIndex);
 
   if (randomPrompt === prompt) return getRandomPrompt(prompt);
   return randomPrompt;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export {getRandomPrompt}
