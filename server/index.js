804ade0edf11c67e7b45a57320364dc4e05c48b8
Danish Manhas <manhasd6@gmail.com>
2023-12-04 20:06:50 +0530

    Add rotes to server
    :Q

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 
 import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js"
+import dalle from "./routes/dalleRoutes.js"
+
 
 dotenv.config();
 
@@ -11,6 +14,9 @@ const app = express();
 const PORT = 8080;
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalle)
 
 app.get("/", async (req, res) => {
   res.send("Hello from DALL-E");
