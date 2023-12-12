5903297808a83e7b265ed790a3cf59a3df12ce67
Danish Manhas <manhasd6@gmail.com>
2023-12-12 17:30:33 +0530

    Fix aiResponse format

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -23,17 +23,18 @@ router.route("/").get((req, res) => {
 router.route("/").post(async (req, res) => {
   try {
     const prompt = req.body.prompt;
-    console.log("Here's what we recieve in the request", prompt)
+    console.log("Here's what we recieve in the request", prompt);
 
     const aiResponse = await openai.images.generate({
       prompt,
       n: 1,
       size: "1024x1024",
       response_format: "b64_json",
-      model:"dall-e-2"
+      model: "dall-e-2",
     });
-
-    const image = aiResponse.data.data[0].b64_json;
+    console.log(" The response received is ", aiResponse);
+    const image = aiResponse.data[0].b64_json;
+    console.log(" The generated image is probably at ", image);
     res.status(200).json({ photo: image });
   } catch (error) {
     console.log("error from dalle api", error);
