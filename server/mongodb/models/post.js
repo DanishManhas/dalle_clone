479af9cfa95a8faf4b43be622440e6eb70cf2036
Danish Manhas <manhasd6@gmail.com>
2023-12-15 21:00:56 +0530

    Setup cloudinary for image upload

--- server/mongodb/models/post.js
+++ server/mongodb/models/post.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const Post = new mongoose.Schema({
+const PostSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
@@ -16,6 +16,6 @@ const Post = new mongoose.Schema({
 });
 
 
-const PostSchema = mongoose.model('Post', Post);
+const Post = mongoose.model('Post', PostSchema);
 
-export default PostSchema;
+export default Post;
